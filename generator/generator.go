b187dc3d9ea285b5f11266021501a941ad3897f7
Anton Matveenko <antmat@me.com>
2017-12-07 18:18:47 +0300

    feat: random file generator

---
+++ generator/generator.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"math/rand"
+	"bufio"
+	"io"
+)
+
+const (
+	B  uint64 = 1
+	Kb          = B << 10
+	Mb          = Kb << 10
+	Gb          = Mb << 10
+	Tb          = Gb << 10
+	MaxFileSize = 100 * Gb
+
+	MaxLineLength = 1 * Mb
+	BufferSize = 10 * Mb
+)
+
+type Generator interface {
+	Run() error
+}
+
+func NewGenerator(outfile string, lineCount uint64, lineLength uint64) (Generator, error) {
+	if lineLength > MaxLineLength || lineLength == 0 {
+		printErrorAndExit(fmt.Errorf("line length can not be zero or greater than %d (got %d)", MaxLineLength, lineLength))
+	}
+
+	fileSize := lineLength * lineCount
+	if fileSize > MaxFileSize {
+		printErrorAndExit(fmt.Errorf("resulting file size is too big (%d, maximum is %d)", fileSize, MaxLineLength))
+	}
+
+	return &generator{
+		outfile: outfile,
+		lineCount: lineCount,
+		lineLength: lineLength,
+		wr: nil,
+	}, nil
+}
+
+type generator struct {
+	outfile string
+	lineCount uint64
+	lineLength uint64
+
+	wr io.ByteWriter
+}
+
+func (g *generator) appendString() {
+	const letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	lettersCnt := int32(len(letters))
+
+	// each random uint64 is capable of generating 4 random bytes
+	// this can boost random string generation a lot
+	random := rand.Uint64()
+	shift := uint(0)
+	for k := 0; k < int(g.lineLength); k++ {
+		if shift == 64 {
+			// we used all bytes from random number
+			shift = 0
+			random = rand.Uint64()
+		}
+		// use (shift/8) byte from random number
+		idx := (random & (0xff << shift)) >> shift
+		shift = shift + 8
+		g.wr.WriteByte(letters[idx % uint64(lettersCnt)])
+	}
+	g.wr.WriteByte('\n')
+}
+
+func(g *generator) Run() error {
+	file, err := os.Create(g.outfile)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	g.wr = bufio.NewWriter(file)
+
+	for line := uint64(0); line < g.lineCount; line++{
+		g.appendString()
+	}
+	return nil
+}
---
+++ generator/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"errors"
+	"os"
+	"strconv"
+	"fmt"
+)
+
+func printErrorAndExit(err error) {
+	fmt.Printf("Error: %s\n", err)
+	fmt.Println("Usage: ./generator {outfile} {line_count} {line_length}")
+	os.Exit(1)
+}
+
+func main() {
+	if len(os.Args) != 4 {
+		printErrorAndExit(errors.New("invalid argument count"))
+	}
+	outfile := os.Args[1]
+	lineCountStr := os.Args[2]
+	lineLengthStr := os.Args[3]
+
+	lineCount, err :=  strconv.ParseUint(lineCountStr, 10, 64)
+	if err != nil {
+		printErrorAndExit(err)
+	}
+
+	lineLength, err := strconv.ParseUint(lineLengthStr, 10, 64)
+	if err != nil {
+		printErrorAndExit(err)
+	}
+
+	g, err := NewGenerator(outfile, lineCount, lineLength)
+	if err != nil {
+		printErrorAndExit(err)
+	}
+
+	err = g.Run()
+	if err != nil {
+		printErrorAndExit(err)
+	}
+}
